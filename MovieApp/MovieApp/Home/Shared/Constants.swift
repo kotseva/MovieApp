cb6875bd9a5e0eacfd381adb6e1483d9fff9cf0c
Angela Koceva <angelakoceva1@gmail.com>
2025-05-18 19:07:05 +0200

    Added custom fonts

---
+++ MovieApp/MovieApp/Home/Shared/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  MovieApp
+//
+//  Created by Angela Koceva on 18.5.25.
+//
+
+import Foundation
+
+struct Fonts {
+    static let interRegular = "Inter-Regular"
+    static let interLightItalic = "Inter-LightItalic"
+    static let interItalic = "Inter-Italic"
+    static let interBold = "Inter-Bold"
+    static let interSemiBold = "Inter-SemiBold"
+    static let interSemiBoldItalic = "Inter-SemiBoldItalic"
+    static let interBoldItalic = "Inter-BoldItalic"
+    static let interMedium = "Inter-Medium"
+    static let interMediumItalic = "Inter-MediumItalic"
+}
